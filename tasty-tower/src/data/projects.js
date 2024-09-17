a91fb03da525d620c3858bf741e2aa72f15a7f71
c0d3hdz <marquezhernandez.damian@gmail.com>
2024-09-16 22:24:48 -0600

    initial_commit

---
+++ tasty-tower/src/data/projects.js
@@ -0,0 +1,24 @@
+
+export const projects = [
+    {
+        slug: 'project-1',
+        title: 'Proyecto 1',
+        description: 'Descripción detallada del Proyecto 1.',
+        image: '/images/RobloxScreenShot20240703_001637342.png',
+        content: 'Aquí puedes agregar más detalles sobre el Proyecto 1.',
+    },
+    {
+        slug: 'project-2',
+        title: 'Proyecto 2',
+        description: 'Descripción detallada del Proyecto 2.',
+        image: '/images/RobloxScreenShot20240703_001637342.png',
+        content: 'Este es otro ejemplo de un proyecto con su propio contenido detallado.',
+    },
+    {
+        slug: 'project-3',
+        title: 'Proyecto 3',
+        description: 'Descripción detallada del Proyecto 3.',
+        image: '/images/RobloxScreenShot20240703_001637342.png',
+        content: 'Este es otro ejemplo de un proyecto con su propio contenido detallado.',
+    },
+]
